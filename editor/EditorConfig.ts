0883bbdcabdcfe8e7272a5bebe54ccbb2d9985de
Bones <Upsetbaden101@gmail.com>
2026-03-16 20:13:23 -0400

    Release v0.2.14

--- editor/EditorConfig.ts
+++ editor/EditorConfig.ts
@@ -23,7 +23,7 @@ export function prettyNumber(value: number): string {
 }
 
 export class EditorConfig {
-    public static readonly version: string = "0.2.13"; // Not using patch versions in display right now, maybe someday.
+    public static readonly version: string = "0.2.14"; // Not using patch versions in display right now, maybe someday.
     public static readonly versionDisplayName: string = "BoneBox - " + EditorConfig.version;
 
     public static readonly releaseNotesURL: string = "https://jummbus.bitbucket.io/patch_notes/" + EditorConfig.version + ".html";
